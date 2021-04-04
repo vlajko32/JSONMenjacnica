a7a7fe60eb7c03ebf52e7b7d0a22cc7739361b2d
vlajko32 <vlajko247@gmail.com>
2021-04-04 13:27:27 +0200

    Kreirana klasa Transakcija u paketu transakcija i u nju dodati svi potrebni atributi kao i odgovarajuce get i set metode

---
+++ src/main/java/transakcija/Transakcija.java
@@ -0,0 +1,66 @@
+package transakcija;
+
+import java.util.Date;
+
+public class Transakcija {
+
+	public String izvornaValuta;
+
+	public String krajnjaValuta;
+
+	public double pocetniIznos;
+
+	public double konvertovaniIznos;
+
+	public Date datumTransakcije;
+
+	public String getIzvornaValuta() {
+		return izvornaValuta;
+	}
+
+	public void setIzvornaValuta(String izvornaValuta) {
+		this.izvornaValuta = izvornaValuta;
+	}
+
+	public String getKrajnjaValuta() {
+		return krajnjaValuta;
+	}
+
+	public void setKrajnjaValuta(String krajnjaValuta) {
+		this.krajnjaValuta = krajnjaValuta;
+	}
+
+	public double getPocetniIznos() {
+		return pocetniIznos;
+	}
+
+	public void setPocetniIznos(double pocetniIznos) {
+		this.pocetniIznos = pocetniIznos;
+	}
+
+	public double getKonvertovaniIznos() {
+		return konvertovaniIznos;
+	}
+
+	public void setKonvertovaniIznos(double konvertovaniIznos) {
+		this.konvertovaniIznos = konvertovaniIznos;
+	}
+
+	public Date getDatumTransakcije() {
+		return datumTransakcije;
+	}
+
+	public void setDatumTransakcije(Date datumTransakcije) {
+		this.datumTransakcije = datumTransakcije;
+	}
+
+	@Override
+	public String toString() {
+		return "Transakcija [izvornaValuta=" + izvornaValuta + ", krajnjaValuta=" + krajnjaValuta + ", pocetniIznos="
+				+ pocetniIznos + ", konvertovaniIznos=" + konvertovaniIznos + ", datumTransakcije=" + datumTransakcije
+				+ "]";
+	}
+
+
+
+}
